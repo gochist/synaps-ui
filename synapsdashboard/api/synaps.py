066d01f58c6ce990446fe006473ffc85b8a7c1d5
June Yi <june.yi@samsung.com>
2014-07-11 23:33:03 +0900

    synaps conf

--- synapsdashboard/api/synaps.py
+++ synapsdashboard/api/synaps.py
@@ -1,25 +1,39 @@
 import logging
 import boto
+from boto.regioninfo import RegionInfo
 
 from django.utils.translation import ugettext_lazy as _
 from horizon import exceptions
 from openstack_dashboard.api import keystone
 
+from django.conf import settings
+
+
 LOG = logging.getLogger(__name__)
+
+REGION_NAME = getattr(settings, "SYNAPS_REGION_NAME", "SPCS")
+REGION_ENDPOINT = getattr(settings, "SYNAPS_REGION_ENDPOINT", "synapsapi")
+SSL = getattr(settings, "SYNAPS_SSL", False)
+PORT = getattr(settings, "SYNAPS_PORT", 3776)
+ACCESS_KEY = getattr(settings, "SYNAPS_ADMIN_AWS_ACCESS_KEY_ID", "accesskey")
+SECRET_KEY = getattr(settings, "SYNAPS_ADMIN_AWS_SECRET_ACCESS_KEY", 
+                     "secretkey")
+AUTH_BACKEND = getattr(settings, "SYNAPS_AUTH", "KEYSTONE")
+
 
 def _get_ec2_credentials(request):
     tenant_id = request.user.tenant_id
     
     try:
-        all_keys = keystone.list_ec2_credentials(request, 
+        all_keys = keystone.list_ec2_credentials(request,
                                                  request.user.id)
         keys = None
         for key in all_keys:
             if key.tenant_id == tenant_id:
                 keys = key
         if keys is None:
-            keys = keystone.create_ec2_credentials(request, 
-                                                   request.user.id, 
+            keys = keystone.create_ec2_credentials(request,
+                                                   request.user.id,
                                                    tenant_id)
     except Exception:
         exceptions.handle(request,
@@ -28,9 +42,21 @@ def _get_ec2_credentials(request):
     
     return keys
 
+
+def _get_ec2_credentials_from_ldap(request):
+    pass
+
+
+if AUTH_BACKEND == "LDAP":
+    _get_ec2_credentials = _get_ec2_credentials_from_ldap
+
+
 def get_cw_client(request):
+    region = RegionInfo(name=REGION_NAME, endpoint=REGION_ENDPOINT)
+
     keys = _get_ec2_credentials(request)
-    cw = boto.connect_cloudwatch(aws_access_key_id=keys.access, 
-                                 aws_secret_access_key=keys.secret)
-    LOG.info("cloudwatch connection: %s", cw)
+    cw = boto.connect_cloudwatch(aws_access_key_id=keys.access,
+                                 aws_secret_access_key=keys.secret,
+                                 is_secure=SSL, port=PORT, region=region)
     return cw
+
